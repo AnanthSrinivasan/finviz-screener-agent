68676689d8be26d46e47b06f9b48ba8dfa1a6aee
Ananth <alwaysananth@gmail.com>
2025-08-13 23:24:12 +0200

    Update test_finviz_agent.py
    fix test file

--- test_finviz_agent.py
+++ test_finviz_agent.py
@@ -6,7 +6,7 @@ class TestFinvizAgent(unittest.TestCase):
     def test_fetch_tickers(self):
         # Lightweight test URL
         test_url = "https://finviz.com/screener.ashx?v=111&f=ind_stocksonly,sh_price_o5&ft=4"
-        df = fetch_all_tickers_from_screener(test_url, max_pages=1)
+        df = fetch_all_tickers(test_url, max_pages=1)
         self.assertTrue(len(df) >= 1, "No tickers fetched from Finviz!")
 
 if __name__ == "__main__":
